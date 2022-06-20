b2a248e269b075b2ae83b85ff03a564addf5a87c
josetropalde <josehenrique.tropalde@gmail.com>
2022-06-20 19:00:00 -0300

    reset branch/nextapp

--- assets/js/main/header.js
+++
@@ -1,8 +0,0 @@
-//Menu
-const btnResponsiveMenu = document.querySelector(".responsive-menu");
-
-function activeMenu() {
-  const header = document.querySelector(".section-header");
-  header.classList.toggle("responsive-menu-active");
-}
-btnResponsiveMenu.addEventListener("click", activeMenu);
--- assets/js/main/loader.js
+++
@@ -1,5 +0,0 @@
-const loader = document.querySelector(".loader");
-
-window.addEventListener("load", () => {
-  loader.style.display = "none";
-});
