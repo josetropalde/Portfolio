ea3cfe61b87a516084b877c3d474abf04df83862
josetropalde <josehenrique.tropalde@gmail.com>
2022-06-23 17:36:20 -0300

    fix key

--- myportfolio/src/components/WorkItem.jsx
+++ myportfolio/src/components/WorkItem.jsx
@@ -1,19 +1,19 @@
 import styles from "../../styles/WorkItem.module.css";
-import Image from "next/image";
+
 export default function WorkItem(props) {
   const { image, title, description, tech, url } = props;
   return (
     <div>
       <div className={styles.portfolio__item}>
-        <Image src={image} alt={title} />
+        <img src={image} alt={title} />
         <div className={styles.portfolio__text}>
           <div>
             <h2>{title}</h2>
             <p>{description}</p>
             <div className={styles.portfolio__technology}>
               <ul>
-                {tech.map((item) => {
-                  return <li>{item.name}</li>;
+                {tech.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
                 })}
               </ul>
             </div>
