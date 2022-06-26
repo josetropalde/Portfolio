e551929b3dd4bb9e15dd2289c5377f7a1f72e7db
josetropalde <josehenrique.tropalde@gmail.com>
2022-06-26 13:29:16 -0300

    update style portfolio

--- myportfolio/src/components/Footer.jsx
+++ myportfolio/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 flex justify-center bg-zinc-800 w-full py-4">
+    <div className="fixed bottom-0 left-0 flex justify-center bg-zinc-800 w-full py-4 z-10">
       <div className="text-xs mx-4 bg-zinc-700 rounded shadow-md p-2 ease-in-out duration-300 hover:bg-zinc-600">
         <a
           target="_blank"
